fed83d213d519382aef50ca3d7955dcee849ac82
Daniel Park <intheimageofyeshua@gmail.com>
2025-10-09 15:44:45 +0900

    Create LineItem Scaffold with Relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   after_commit -> { broadcast_refresh_later_to "products" }
   validates :title, :description, :image, presence: true
   validates :title, uniqueness: true
@@ -14,4 +18,14 @@ class Product < ApplicationRecord
     end
   end
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+
+  # ensure that there are not line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
+    end
+  end
 end
